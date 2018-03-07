b62ac828bd700ec4516e1db9b7f3153d52a4293a
Michael Uloth <hello@michaeluloth.com>
2018-03-07 11:24:26 -0500

    Removed flashing when collapsing project details

--- src/sections/Work.js
+++ src/sections/Work.js
@@ -12,7 +12,7 @@ class Work extends React.Component {
     const mergedProjects = this.mergeProjectArrays()
 
     // Add a key to each project (to prevent unnecessary rerendering)
-    const projectsWithKeys = mergedProjects.map(project => {
+    const projectsWithKeys = [...mergedProjects].map(project => {
       project.key = shortid.generate()
       return project
     })
@@ -161,7 +161,7 @@ const Filters = ({ operaIsChecked, websitesIsChecked, handleChange }) => (
 
 const Projects = ({ projects, limit, operaIsChecked, websitesIsChecked }) => {
   // Reduce project list to the active category
-  const projectsInActiveCategory = projects.filter(project => {
+  const projectsInActiveCategory = [...projects].filter(project => {
     if (operaIsChecked && websitesIsChecked) return project
     else if (operaIsChecked) return project.node.category === `Opera`
     else if (websitesIsChecked) return project.node.category === `Website`
@@ -171,16 +171,33 @@ const Projects = ({ projects, limit, operaIsChecked, websitesIsChecked }) => {
   return (
     <ul>
       {/* Reduce visible projects to the current limit */}
-      {projectsInActiveCategory.slice(0, limit).map(project => {
-        return (
-          <li key={project.key}>
-            <Project project={project.node} />
-          </li>
-        )
+      {projectsInActiveCategory.slice(0, limit).map((project, index) => {
+        return <Project key={project.key} project={project.node} />
       })}
     </ul>
   )
 }
+
+// class Projects extends React.Component {
+//   render() {
+//     // Reduce project list to the active category
+//     const projectsInActiveCategory = [...projects].filter(project => {
+//       if (operaIsChecked && websitesIsChecked) return project
+//       else if (operaIsChecked) return project.node.category === `Opera`
+//       else if (websitesIsChecked) return project.node.category === `Website`
+//       else console.error(`Error in projectsInActiveCategory calculation in <Projects />`)
+//     })
+
+//     return (
+//       <ul>
+//         {/* Reduce visible projects to the current limit */}
+//         {projectsInActiveCategory.slice(0, limit).map((project, index) => {
+//           return <Project key={project.key} project={project.node} />
+//         })}
+//       </ul>
+//     )
+//   }
+// }
 
 /* 
  *
@@ -202,43 +219,66 @@ class Project extends React.Component {
   state = { expanded: false }
 
   componentDidMount = () => {
-    // console.log(`pk2`, this.props)
     if (!loadjs.isDefined('gsap')) {
       loadjs('https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js', 'gsap')
     }
   }
 
-  handleClick = () => {
-    if (this.state.expanded) {
-      this.setState({ expanded: false })
-      this.collapse()
-    } else {
-      this.setState({ expanded: true })
-      this.expand()
-    }
-  }
+  handleClick = () => (this.state.expanded ? this.collapse() : this.expand())
 
   expand = () => {
+    loadjs.ready('gsap', () => {
+      // When expanding, set this immediately
+      this.setState({ expanded: true })
+
+      // Invalidate the temporary inline styles (which match the starting state for the animation and are added to prevent a flash of content in the ending position)
+      this.item.style = null
+
+      // Expand the section to its natural height
+      TweenMax.to(this.item, 1, {
+        height: this.item.offsetHeight,
+        autoAlpha: 1,
+        ease: Power3.easeInOut
+      })
+
+      // Store tween in DOM node
+      // this.item.animation = expand
+    })
+  }
+
+  // collapse = () => this.item.animation.reverse()
+
+  collapse = () => {
     loadjs.ready('gsap', () => {
       // Invalidate the temporary inline styles (which match the starting state for the animation and are added to prevent a flash of content in the ending position)
       this.item.style = null
 
-      const expand = TweenMax.to(this.item, 1, {
-        height: this.item.offsetHeight,
-        autoAlpha: 1,
-        ease: Power4.easeInOut
+      // Collapse the section to 0
+      TweenMax.to(this.item, 1, {
+        height: 0,
+        autoAlpha: 0,
+        ease: Power3.easeInOut,
+        // When collapsing, set this after animation completes
+        onComplete: () => this.setState({ expanded: false })
       })
-
-      // Store tween in DOM node
-      this.item.animation = expand
     })
   }
 
-  collapse = () => {
-    loadjs.ready('gsap', () => this.item.animation.reverse())
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate', nextState === this.state)
+    if (nextState !== this.state) return true
+    else return false
+    // console.log(`nextProps`, nextProps)
+    // console.log(`nextState`, nextState)
+
+    // if (nextProps.test === this.props.test) return false
+    // else return true
   }
 
   render() {
+    const { key, project } = this.props
+    const { expanded } = this.state
+
     return (
       <div
         role="button"
@@ -249,13 +289,13 @@ class Project extends React.Component {
       >
         <span class="sr-only">Click to see project details</span>
         <div class="pv4 hover:bg-near-white animate">
-          <ProjectHeader project={this.props.project} expanded={this.state.expanded} />
+          <ProjectHeader project={project} expanded={expanded} />
           <div
             ref={el => (this.item = el)}
             class="relative z-2 overflow-hidden"
             style={{ height: 0 }}
           >
-            <ProjectDetails project={this.props.project} />
+            <ProjectDetails project={project} />
           </div>
         </div>
       </div>
@@ -306,80 +346,87 @@ const ProjectHeader = ({ project, expanded }) => (
 import HyperLink from '../components/HyperLink'
 import Img from '../components/Img'
 
-const ProjectDetails = ({ project }) => (
-  <div class="container pt4 lh-tall">
-    <div
-      class="mb4 "
-      style={{
-        display: `grid`,
-        gridTemplateColumns: `repeat(auto-fit, minmax(200px, 1fr)`,
-        gridGap: `1rem`
-      }}
-    >
-      {/* Remove the "images" loop? */}
-      {project.images ? (
-        project.images.map(photo => {
-          return (
-            <Img
-              key={shortid()}
-              sizes={photo.image.childImageSharp.sizes}
-              alt={photo.alt}
-              critical={true}
-              className="shadow-lg"
-            />
-          )
-        })
-      ) : (
-        <figure>
-          <Img
-            sizes={project.image.childImageSharp.sizes}
-            alt={project.alt}
-            critical={true}
-            className="shadow-lg"
-          />
-          <figcaption class="o-50 pt1 f6">{project.alt}</figcaption>
-        </figure>
-      )}
-    </div>
+class ProjectDetails extends React.Component {
+  shouldComponentUpdate = () => false
 
-    {project.reviews &&
-      project.reviews.map(review => {
-        return (
-          <blockquote key={shortid()} class="pv1">
-            <div class="bw3 bt-0 br-0 bb-0 b--green pl3 measure">
-              <p class="mb2 f3">{review.quotation}</p>
-              {review.link ? (
-                <cite class="f4 fw7 fs-normal">
-                  <HyperLink href={review.link} className="">
-                    {review.source}
-                  </HyperLink>
-                </cite>
-              ) : (
-                <cite class="">&mdash; {review.source}</cite>
-              )}
-            </div>
-          </blockquote>
-        )
-      })}
+  render() {
+    const { project } = this.props
+    return (
+      <div class="container pt4 lh-tall">
+        <div
+          class="mb4 "
+          style={{
+            display: `grid`,
+            gridTemplateColumns: `repeat(auto-fit, minmax(200px, 1fr)`,
+            gridGap: `1rem`
+          }}
+        >
+          {/* Remove the "images" loop? */}
+          {project.images ? (
+            project.images.map(photo => {
+              return (
+                <Img
+                  key={shortid()}
+                  sizes={photo.image.childImageSharp.sizes}
+                  alt={photo.alt}
+                  critical={true}
+                  className="shadow-lg"
+                />
+              )
+            })
+          ) : (
+            <figure>
+              <Img
+                sizes={project.image.childImageSharp.sizes}
+                alt={project.alt}
+                critical={true}
+                className="shadow-lg"
+              />
+              <figcaption class="o-50 pt1 f6">{project.alt}</figcaption>
+            </figure>
+          )}
+        </div>
 
-    <p class="mb4 measure">{project.description}</p>
-
-    <div class="mb4 ">
-      {project.details &&
-        project.details.map(detail => {
-          return (
-            detail.name !== `Dates` && (
-              <dl key={shortid()}>
-                <dt class="dib fw7">{detail.name}:&nbsp;</dt>
-                <dd class="dib">{detail.value}</dd>
-              </dl>
+        {project.reviews &&
+          project.reviews.map(review => {
+            return (
+              <blockquote key={shortid()} class="pv1">
+                <div class="bw3 bt-0 br-0 bb-0 b--green pl3 measure">
+                  <p class="mb2 f3">{review.quotation}</p>
+                  {review.link ? (
+                    <cite class="f4 fw7 fs-normal">
+                      <HyperLink href={review.link} className="">
+                        {review.source}
+                      </HyperLink>
+                    </cite>
+                  ) : (
+                    <cite class="">&mdash; {review.source}</cite>
+                  )}
+                </div>
+              </blockquote>
             )
-          )
-        })}
-    </div>
+          })}
 
-    <HyperLink href={project.link} className="link mb4 tc">
-      View site →
-    </HyperLink>
-  </div>
-)
+        <p class="mb4 measure">{project.description}</p>
+
+        <div class="mb4 ">
+          {project.details &&
+            project.details.map(detail => {
+              return (
+                detail.name !== `Dates` && (
+                  <dl key={shortid()}>
+                    <dt class="dib fw7">{detail.name}:&nbsp;</dt>
+                    <dd class="dib">{detail.value}</dd>
+                  </dl>
+                )
+              )
+            })}
+        </div>
+
+        <HyperLink href={project.link} className="link mb4 tc">
+          View site →
+        </HyperLink>
+      </div>
+    )
+  }
+}
