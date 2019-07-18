465971060cd8c5a84e3e48c4ed61f9be907b06c9
Michael Uloth <hello@michaeluloth.com>
2019-07-18 17:51:38 -0400

    feat: replace react-icons with gatsby-plugin-svgr

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,128 +1,151 @@
-const Contact = () => (
-  <section className="bg-near-white pv6 avenir">
-    <div className="container">
-      <Invitation />
-      <ContactLinks />
+function Contact() {
+  return (
+    <section className="bg-near-white pv6 avenir">
+      <div className="container">
+        <Invitation />
+        <ContactLinks />
+      </div>
+    </section>
+  )
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+
+function Invitation() {
+  return (
+    <Fragment>
+      <h2
+        className="mb4 lh-solid f-3p5 sm:f-5 md:f-6 fw9 ttu"
+        style={{
+          marginLeft: `-.03em`
+        }}
+      >
+        Contact
+        <span aria-hidden="true" className="green">
+          .
+        </span>
+      </h2>
+
+      <p
+        className="mb4 pb3 measure-narrow lh-copy f4 sm:f3 fw4"
+        style={{ maxWidth: `32ch` }}
+      >
+        Want to work together? Tell me about your project!
+        {` `}
+        <Emoji emoji="👋" ariaLabel="Emoji of a hand waving" />
+      </p>
+    </Fragment>
+  )
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+
+function ContactLinks() {
+  return (
+    <div className="sm:flex sm:items-center">
+      <Link
+        href="mailto:hello@michaeluloth.com"
+        className="link dib mr3 mb4 sm:mb0"
+      >
+        Email me
+      </Link>
+
+      <SocialLinks />
     </div>
-  </section>
-)
+  )
+}
 
-/*************
- *
- * Invitation
- *
- ************/
+///////////////////////////////////////////////////////////////////////////////////
 
-const Invitation = () => (
-  <Fragment>
-    <h2
-      className="mb4 lh-solid f-3p5 sm:f-5 md:f-6 fw9 ttu"
-      style={{
-        marginLeft: `-.03em`
-      }}
-    >
-      Contact
-      <span aria-hidden="true" className="green">
-        .
-      </span>
-    </h2>
-
-    <p
-      className="mb4 pb3 measure-narrow lh-copy f4 sm:f3 fw4"
-      style={{ maxWidth: `32ch` }}
-    >
-      Want to work together? Tell me about your project!
-      {` `}
-      <Emoji emoji="👋" ariaLabel="Emoji of a hand waving" />
-    </p>
-  </Fragment>
-)
-
-/***************
- *
- * Contact Links
- *
- ***************/
-
-const ContactLinks = () => (
-  <div className="sm:flex sm:items-center">
-    <Link href="mailto:hello@michaeluloth.com" className="link dib mr3 mb4 sm:mb0">
-      Email me
-    </Link>
-
-    <SocialLinks />
-  </div>
-)
-
-/*
- *
- * Social Links
- *
- */
-
-const SocialLinks = () => (
-  <nav aria-label="Social media links" className="">
-    <ul>
-      {links.map(link => {
-        return (
-          <li key={link.name} className="dib mr3 f3">
-            <Link
-              href={link.url}
-              className="icon"
-              srText={`Follow me on ${link.name}`}
-            >
-              {link.icon}
-            </Link>
-          </li>
-        )
-      })}
-    </ul>
-  </nav>
-)
+function SocialLinks() {
+  return (
+    <nav aria-label="Social media links" className="">
+      <ul>
+        {links.map(link => {
+          return (
+            <li key={link.name} className="dib mr3 f3">
+              <Link
+                href={link.url}
+                className="icon"
+                srText={`Follow me on ${link.name}`}
+              >
+                {link.icon}
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  )
+}
 
 const links = [
   {
     name: `Twitter`,
     icon: (
-      <FaTwitter className="" style={{ width: `1.608rem`, height: `1.5rem` }} />
+      <TwitterSVG
+        style={{ width: `1.608rem`, height: `1.5rem`, verticalAlign: `middle` }}
+      />
     ),
     url: `https://twitter.com/ooloth`
   },
   {
-    name: `Facebook`,
-    icon: (
-      <FaFacebook className="" style={{ width: `1.608rem`, height: `1.5rem` }} />
-    ),
-    url: `https://www.facebook.com/michaeluloth`
-  },
-  {
     name: `LinkedIn`,
     icon: (
-      <FaLinkedin className="" style={{ width: `1.608rem`, height: `1.5rem` }} />
+      <LinkedInSVG
+        style={{ width: `1.608rem`, height: `1.5rem`, verticalAlign: `middle` }}
+      />
     ),
     url: `https://www.linkedin.com/in/michael-uloth-848a1b98/`
   },
   {
     name: `GitHub`,
     icon: (
-      <FaGithub className="" style={{ width: `1.608rem`, height: `1.5rem` }} />
+      <GitHubSVG
+        style={{ width: `1.608rem`, height: `1.5rem`, verticalAlign: `middle` }}
+      />
     ),
     url: `https://github.com/ooloth`
+  },
+  {
+    name: `Facebook`,
+    icon: (
+      <FacebookSVG
+        style={{
+          width: `1.608rem`,
+          height: `1.5rem`,
+          verticalAlign: `middle`,
+          padding: `.035rem 0`
+        }}
+      />
+    ),
+    url: `https://www.facebook.com/michaeluloth`
+  },
+  {
+    name: `Dev.to`,
+    icon: (
+      <DevSVG
+        style={{
+          width: `1.608rem`,
+          height: `1.5rem`,
+          verticalAlign: `middle`,
+          transform: `scale(1.25)`
+        }}
+      />
+    ),
+    url: `https://dev.to/ooloth`
   }
 ]
 
-/*
- *
- * Imports & Exports
- *
- */
+///////////////////////////////////////////////////////////////////////////////////
 
 import React, { Fragment } from 'react'
 
-import FaTwitter from 'react-icons/lib/fa/twitter'
-import FaGithub from 'react-icons/lib/fa/github'
-import FaLinkedin from 'react-icons/lib/fa/linkedin'
-import FaFacebook from 'react-icons/lib/fa/facebook'
+import { ReactComponent as TwitterSVG } from '../svg/twitter-brands.svg'
+import { ReactComponent as GitHubSVG } from '../svg/github-brands.svg'
+import { ReactComponent as DevSVG } from '../svg/dev-brands.svg'
+import { ReactComponent as LinkedInSVG } from '../svg/linkedin-in-brands.svg'
+import { ReactComponent as FacebookSVG } from '../svg/facebook-f-brands.svg'
 
 import Emoji from '../components/Emoji'
 import Link from '../components/Link'
